df5871b6097590c4cf556c9617ad576282dcd278
gitCchub <996526301@qq.com>
2017-10-09 20:44:02 +0800

    add client

--- client.c
+++ client.c
@@ -19,32 +19,36 @@
 static void handle_connection(int cli_sock);
 static void
 handle_event(int epollfd, struct epoll_event *events, int num, int cli_sock, char *buf);
-static void sendata(int epollfd, int fd, int cli_sock, char *buf);
-static void recdata(int epollfd, int fd, int cli_sock, char *buf);
-//static void readata(int epollfd, int fd, int cli_sock, char *buf);
-//static void writedata(int epollfd, int fd, int cli_sock, char *buf);
+static void readata(int epollfd, int fd, int cli_sock, char *buf);
+static void writedata(int epollfd, int fd, int cli_sock, char *buf);
 static void add_event(int epollfd, int fd, int state);
 static void delete_event(int epollfd, int fd, int state);
 static void modify_event(int epollfd, int fd, int state);
 
 int main(){
-    int cli_sock = socket(AF_INET, SOCK_STREAM, 0);
-      if(cli_sock < 0){
-          perror("socket error");
+    int cli_sock, connect_fd;
+    if((cli_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+          printf("create socket error: %s(errno: %d)\n", strerror(errno), errno);
           return 1;
-      }
+    };
     struct sockaddr_in  seraddr;
     bzero(&seraddr, sizeof(seraddr));
     seraddr.sin_family = AF_INET;
     seraddr.sin_port = htons(PORT);
-    inet_pton(AF_INET, IPADDRESS, &seraddr.sin_addr);
-    int connect_fd = connect(cli_sock, (struct sockaddr*)&seraddr, sizeof(seraddr));
-    if (connect_fd < 0){
-        perror("connect error");
-        return 2;
+    if(inet_pton(AF_INET, IPADDRESS, &seraddr.sin_addr) <= 0){
+        printf("inet_pton error for %s\n", IPADDRESS);
+        return 1;
     };
+    if ((connect_fd = connect(cli_sock, (struct sockaddr*)&seraddr, sizeof(seraddr))) == -1){
+        printf("connect error: %s(errno: %d)\n", strerror(errno), errno);
+        return 2;
+    }
     handle_connection(cli_sock);
+    printf("connect: %d\n", connect_fd);
+    printf("sock: %d", cli_sock);
     close(cli_sock);
+    printf("connect: %d\n", connect_fd);
+    printf("sock: %d\n", cli_sock);
     return 0;
 }
 
@@ -68,35 +72,39 @@ handle_event(int epollfd, struct epoll_event *events, int num, int cli_sock, cha
     int i;
     for (i = 0; i < num; i++){
         fd = events[i].data.fd;
-        if (events[i].events & EPOLLIN)
-            sendata(epollfd, fd, cli_sock, buf);
-        else if (events[i].events & EPOLLOUT)
-            recdata(epollfd, fd, cli_sock, buf);
-    }
-}
-
-static void sendata(int epollfd, int fd, int cli_sock, char *buf){
-    //char sendline[1024];
-    printf("send msg \n");
-    fgets(buf, 1024, stdin);
-    if(send(fd, buf, strlen(buf), 0) < 0){
-        printf("send error: %s(errno: %d)\n", strerror(errno), errno);
-        exit(0);
-    }else{
-        if (fd == STDIN_FILENO)
-            add_event(epollfd, cli_sock, EPOLLOUT);
-        else{
-            delete_event(epollfd, cli_sock, EPOLLIN);
-            add_event(epollfd, STDOUT_FILENO, EPOLLOUT);
+        if (events[i].events & EPOLLIN){
+            readata(epollfd, fd, cli_sock, buf);
+        }
+        else if (events[i].events & EPOLLOUT){
+            writedata(epollfd, fd, cli_sock, buf);
         }
     }
 }
 
-static void recdata(int epollfd, int fd, int cli_sock, char *buf){
-    ssize_t rec_len;
-    if((rec_len = recv(fd, buf, SIZE, 0)) == -1) {
-       perror("receive error");
-       exit(1);
+ static void readata(int epollfd, int fd, int cli_sock, char *buf){
+     ssize_t read_data = read(fd, buf, SIZE);
+     if (read_data == -1){
+         perror("read error");
+         close(fd);
+     }
+     else if (read_data == 0){
+         fprintf(stderr, "server close.\n");
+         close(fd);
+     }else{
+         if (fd == STDIN_FILENO)
+             add_event(epollfd, cli_sock, EPOLLOUT);
+         else{
+             delete_event(epollfd, cli_sock, EPOLLIN);
+             add_event(epollfd, STDOUT_FILENO, EPOLLOUT);
+         }
+     }
+ }
+
+static void writedata(int epollfd, int fd, int cli_sock, char *buf){
+    ssize_t write_data = write(fd, buf, strlen(buf));
+    if (write_data == -1){
+        perror("write error");
+        close(fd);
     }else{
         if (fd == STDOUT_FILENO)
             delete_event(epollfd, fd, EPOLLOUT);
@@ -104,40 +112,8 @@ static void recdata(int epollfd, int fd, int cli_sock, char *buf){
             modify_event(epollfd, fd, EPOLLIN);
     }
     memset(buf, 0, SIZE);
-}
-
-// static void readata(int epollfd, int fd, int cli_sock, char *buf){
-//     ssize_t read_data = read(fd, buf, SIZE);
-//     if (read_data == -1){
-//         perror("read error");
-//         close(fd);
-//     }
-//     else if (read_data == 0){
-//         fprintf(stderr, "server close.\n");
-//         close(fd);
-//     }else{
-//         if (fd == STDIN_FILENO)
-//             add_event(epollfd, cli_sock, EPOLLOUT);
-//         else{
-//             delete_event(epollfd, cli_sock, EPOLLIN);
-//             add_event(epollfd, STDOUT_FILENO, EPOLLOUT);
-//         }
-//     }
-// }
-
-// static void writedata(int epollfd, int fd, int cli_sock, char *buf){
-//     ssize_t write_data = write(fd, buf, strlen(buf));
-//     if (write_data == -1){
-//         perror("write error");
-//         close(fd);
-//     }else{
-//         if (fd == STDOUT_FILENO)
-//             delete_event(epollfd, fd, EPOLLOUT);
-//         else
-//             modify_event(epollfd, fd, EPOLLIN);
-//     }
-//     memset(buf, 0, SIZE);
-// }
+    buf[SIZE] = '\0';
+ }
 
 static void add_event(int epollfd, int fd, int state){
     struct epoll_event ev;
